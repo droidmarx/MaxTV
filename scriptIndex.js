39af2278594f87c51650adc08b9cb7565eea0735
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2025-02-23 12:28:51 -0300

    Atualizar o scriptIndex.js

--- scriptIndex.js
+++ scriptIndex.js
@@ -50,14 +50,28 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     const passwordInput = document.getElementById("password");
     const eyeIcon = document.getElementById("eye-icon");
+    const togglePassword = document.querySelector(".toggle-password");
 
+    // Oculta o ícone no início
+    togglePassword.style.display = "none";
+
+    // Mostra o ícone apenas quando houver algo digitado
+    passwordInput.addEventListener("input", () => {
+        if (passwordInput.value.length > 0) {
+            togglePassword.style.display = "block";
+        } else {
+            togglePassword.style.display = "none";
+        }
+    });
+
+    // Alterna a visibilidade da senha ao clicar no ícone
     eyeIcon.addEventListener("click", () => {
         if (passwordInput.type === "password") {
             passwordInput.type = "text";
-            eyeIcon.src = "https://img.icons8.com/ios-glyphs/30/ffffff/invisible.png";
+            eyeIcon.src = "https://img.icons8.com/?size=128&id=3seXONfwoB83&format=png";
         } else {
             passwordInput.type = "password";
-            eyeIcon.src = "https://img.icons8.com/ios-glyphs/30/ffffff/visible.png";
+            eyeIcon.src = "https://img.icons8.com/?size=128&id=0R7F3PxtxHVm&format=png";
         }
     });
 });
